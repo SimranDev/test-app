2b3c69bf69bf8afdb47b6efdf5f32b0f5b2c4e2c
SimranDev <simran.develop@gmail.com>
2021-04-17 20:37:56 +0530

    test1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const memory = navigator.deviceMemory;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="fb-root">
+      <div>
+        <h2>Memory: {memory * 2}</h2>
+      </div>
     </div>
   );
 }
