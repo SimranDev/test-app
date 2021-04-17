74f4750c64fd33e67fe3d6f9171429f83cdc3670
SimranDev <simran.develop@gmail.com>
2021-04-17 20:52:12 +0530

    useAgent API added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
   const memory = navigator.deviceMemory;
+  const geo = navigator.geolocation.getCurrentPosition;
+  var ua = navigator.userAgent;
+
+  useEffect(() => {
+    console.log(geo);
+  });
 
   return (
     <div className="App" id="fb-root">
       <div>
         <h2>Memory: {memory * 2}</h2>
+        <h2>{ua}</h2>
       </div>
     </div>
   );
